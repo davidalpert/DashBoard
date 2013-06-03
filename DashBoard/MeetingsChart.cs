c248447c52c8a591b434d056102d2b5ce14e5943
Llewellyn Falco <isidore@setgame.com>
2013-06-03 00:35:23 -0500

    Meetings

---
+++ DashBoard/MeetingsChart.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DashBoard
+{
+	public partial class MeetingsChart : Form
+	{
+		private readonly Meeting[] meetings;
+		private int barWidth=10;
+		private int barHeight=600;
+		private double minutesPerDay=60*24;
+		private int numberOfDays = 2*4*7;
+
+		public MeetingsChart(Meeting[] meetings)
+		{
+			this.meetings = meetings;
+			InitializeComponent();
+			this.Width= (barWidth+1)*numberOfDays;
+			this.Height = barHeight + 60;
+			FormBorderStyle = FormBorderStyle.None;
+		}
+
+		protected override void OnPaint(PaintEventArgs e)
+		{
+			base.OnPaint(e);
+			DrawBackgrounds(e.Graphics);
+			for (int i = 0; i < numberOfDays; i++)
+			{
+				PaintDay(e.Graphics, i);
+			}
+		}
+
+		private void DrawBackgrounds(Graphics g)
+		{
+			for (int day = 0; day < numberOfDays; day++)
+			{
+				DrawBackground(g, day);
+			}
+		}
+		private void DrawBackground(Graphics g, int day)
+		{
+			int startX = GetStartXForDay(day);
+			if (IsWeekend(day))
+			{
+				g.FillRectangle(Brushes.DarkGray, startX, 0, barWidth, barHeight);
+				g.FillRectangle(Brushes.Firebrick, startX, ScaleHeight(8 * 60), barWidth, ScaleHeight((9) * 60));
+			}
+			else
+			{
+				g.FillRectangle(Brushes.DarkGray, startX, 0, barWidth, barHeight);
+				g.FillRectangle(Brushes.LightGray, startX, ScaleHeight(8 * 60), barWidth, ScaleHeight(9 * 60));
+			}
+				}
+
+		private bool IsWeekend(int day)
+		{
+			int dayOfWeek = day%7;
+			return (dayOfWeek == 0 || dayOfWeek == 6);
+		}
+
+		private void PaintDay(Graphics g, int day)
+		{
+			int startX = GetStartXForDay(day);
+			foreach (var meeting in meetings.Where(x=>x.day == day))
+			{
+				g.FillRectangle(Brushes.Black,startX, ScaleHeight(meeting.minute),barWidth, meeting.Length);
+			}
+		}
+
+		private int GetStartXForDay(int day)
+		{
+			return day*(barWidth +1);
+		}
+
+		private int ScaleLinesOfCode(int linesOfCode)
+		{
+			return (int) Math.Log10(linesOfCode*barWidth/Math.E)*3;
+		}
+
+
+		private int ScaleHeight(int minute)
+		{
+			double percent = (minute/minutesPerDay);
+			return (int) (percent*barHeight);
+		}
+	}
+}
--- DashBoard/MeetingsTest.cs
+++ DashBoard/MeetingsTest.cs
@@ -4,18 +4,22 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace DashBoard
 {
 	[TestClass]
-	[UseReporter(typeof(DiffReporter))]
 	public class MeetingsTest
 	{
 	
 		[TestMethod]
 		public void TestMeetings()
 		{
-			var checkins = new[] { new Meeting(1, 9, 30, 10, 5)
-				, new Meeting(1, 8, 30, 10, 6)
-				, new Meeting(1, 19, 30, 60, 3) }; 
+			var meetings = new[] { 
+				new Meeting(1, 8, 0, 10, 5),
+				new Meeting(2, 8, 0, 10, 5),
+				new Meeting(3, 8, 0, 10, 5),
+				new Meeting(4, 8, 0, 10, 5),
+				new Meeting(5, 8, 0, 10, 5),
+				new Meeting(1, 13, 0, 60, 3),
+				new Meeting(5, 10, 30, 60, 3) }; 
 
-			//ApprovalTests.WinForms.WinFormsApprovals.Verify(new CheckInChart(checkins));
+			ApprovalTests.WinForms.WinFormsApprovals.Verify(new MeetingsChart(meetings));
 
 		}
 	}
